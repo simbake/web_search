200ed2df45ffa564b6fe2ebf070f9d59c89f3fc6
AIWintermuteAI <dmitrywat@gmail.com>
2023-11-17 17:52:08 +0100

    use regex for search inform user if no results are found

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import urllib
+import re
 
 search_access = True
 service = Service()
@@ -16,7 +17,7 @@ options.add_argument('--disable-gpu')
 options.add_argument('--remote-debugging-port=9222')
 
 def google_results(query):
-    
+
     driver = webdriver.Chrome(service=service,options=options)
     query = urllib.parse.quote_plus(query)
     url="https://www.google.com/search?hl=en&q="+query
@@ -25,37 +26,44 @@ def google_results(query):
     driver.quit()
     return html
 
-
 def ui():
     global search_access
     checkbox = gr.Checkbox(value=search_access, label="Enable Google Search")
     checkbox.change(fn=update_search_access, inputs=checkbox)
     return checkbox, search_access
 
-
 def update_search_access(checkbox_value):
     global search_access
     search_access = checkbox_value  # assign the value of the checkbox to the variable
     return search_access, checkbox_value
 
-
 def input_modifier(user_input, state):
     global search_access
     if search_access:
-        if user_input.lower().startswith("search"):
-            shared.processing_message = "*Searching online...*"
+        search_query = re.search(r'search\s+"([^"]+)"', user_input, re.IGNORECASE)
+        if search_query:
+            query = search_query.group(1)
+        elif user_input.lower().startswith("search"):
             query = user_input.replace("search", "").strip()
-            state["context"] = state["context"] + "Relevant search results are in the Google search results. Use this info in the response."
-            search_data = google_results(query) 
-            user_prompt = f"User question: {user_input}\n Google search results: {search_data}"
-            return str(user_prompt)               
+
+        if not query:
+            shared.processing_message = "*Typing...*"
+            return user_input
+        else:
+            shared.processing_message = f"*Searching online for {query}*"
+            state["context"] = state["context"] + "Relevant search results are in the Google search results. Use this info in the response. If no results are found, say so and offer information from the memory."
+            search_data = google_results(query)
+            if not search_data:
+                user_prompt = f"User question: {user_input}\n Google search results: NO RESULTS FOUND"
+            else:
+                user_prompt = f"User question: {user_input}\n Google search results: {search_data}"
+            return str(user_prompt)
     shared.processing_message = "*Typing...*"
     return user_input
 
 
 def output_modifier(output):
     return output
-
 
 def bot_prefix_modifier(prefix):
     return prefix
